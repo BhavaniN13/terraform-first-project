4a1c88bd45cf70d74a789849820f0e4598e2c510
BhavaniN13 <bhavanin.n6@gmail.com>
2025-07-16 10:31:12 +0000

    vnet

--- resource.tf
+++ resource.tf
@@ -1,18 +1,24 @@
-resource "azurerm_resource_group" "storeterra" {
-    name     = "terra-RG"
-    location = "East US"
+resource "azurerm_resource_group" "terra" {
+  name     = "terra"
+  location = "East US"
+}
+resource "azurerm_virtual_network" "terra_vnet" {
+    name                = "terra-vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = azurerm_resource_group.terra.location
+    resource_group_name = azurerm_resource_group.terra.name
 }
  
-resource "azurerm_storage_account" "saterra" {
-    name                     = "terranb"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_subnet" "subnet1" {
+    name                 = "subnet1"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
 }
  
-resource "azurerm_storage_container" "sacontainer" {
-    name                  = "terra-container"
-    storage_account_id  = azurerm_storage_account.saterra.id
-    container_access_type = "private"
+resource "azurerm_subnet" "subnet2" {
+    name                 = "subnet2"
+    resource_group_name  = azurerm_resource_group.terra.name
+    virtual_network_name = azurerm_virtual_network.terra_vnet.name
+    address_prefixes     = ["10.0.2.0/24"]
 }
